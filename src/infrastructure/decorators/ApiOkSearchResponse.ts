ae9f5f574d8bf81da94d9673d0b51dff50d2a4c3
perlexed <perlexed@gmail.com>
2022-02-16 20:13:24 +0700

    v1.1.11 ApiOkSearchResponse params made comply with ApiOkResponse

--- src/infrastructure/decorators/ApiOkSearchResponse.ts
+++ src/infrastructure/decorators/ApiOkSearchResponse.ts
@@ -2,7 +2,12 @@ import {applyDecorators} from '@nestjs/common';
 import {ApiExtraModels, ApiOkResponse, getSchemaPath} from '@nestjs/swagger';
 import {SearchSchema} from '../schemas/SearchSchema';
 
-export function ApiOkSearchResponse(itemType) {
+interface ApiOkSearchResponseOptions {
+    type: Function,
+}
+
+export function ApiOkSearchResponse(options: ApiOkSearchResponseOptions) {
+    const itemType = options.type;
     return applyDecorators(
         ApiExtraModels(SearchSchema, itemType),
         ApiOkResponse({
