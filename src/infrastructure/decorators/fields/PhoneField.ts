e15bd5369bd9b4ed61fce5423a1e6a23d5dea8b0
Nick Likhodievsky <nicklihorus4@gmail.com>
2022-02-22 15:20:29 +0700

    add transform to PhoneField

--- src/infrastructure/decorators/fields/PhoneField.ts
+++ src/infrastructure/decorators/fields/PhoneField.ts
@@ -2,10 +2,13 @@ import {applyDecorators} from '@nestjs/common';
 import {Column} from 'typeorm';
 import {IsPhoneNumber} from 'class-validator';
 import {BaseField, IBaseFieldOptions} from './BaseField';
+import {Transform} from "class-transformer";
 
 export interface IPhoneFieldOptions extends IBaseFieldOptions {
     unique?: boolean,
 }
+
+const findCharsExceptPhoneSymbolsRegExp = /[^+\d]/g;
 
 export function PhoneField(options: IPhoneFieldOptions = {}) {
     if (!options.label) {
@@ -25,6 +28,7 @@ export function PhoneField(options: IPhoneFieldOptions = {}) {
             unique: options.unique,
             nullable: options.nullable,
         }),
+        Transform(({value}) => value ? value.replace(findCharsExceptPhoneSymbolsRegExp, '') : value),
         IsPhoneNumber(null, {
             message: 'Некорректный номер телефона',
         }),
