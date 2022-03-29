812f9d7980a400105eeea13f01c385f66632c87a
Vladimir Kozhin <affka@affka.ru>
2022-03-29 11:33:05 +0700

    1.3.15 Поменял создание дто в CreateDtoPipe с plainToInstance на DataMapper. Потому что до этого в дто писались все подряд поля, которые приходили с фронтенда

--- src/infrastructure/pipes/CreateDtoPipe.ts
+++ src/infrastructure/pipes/CreateDtoPipe.ts
@@ -1,9 +1,13 @@
 import {ArgumentMetadata, Injectable, PipeTransform} from '@nestjs/common';
+import {DataMapper} from '../../usecases/helpers/DataMapper';
+import {isMetaClass} from '../decorators/fields/BaseField';
 import {plainToInstance} from 'class-transformer';
 
 @Injectable()
 export class CreateDtoPipe implements PipeTransform<any> {
     async transform(value: any, metadata: ArgumentMetadata): Promise<any> {
-        return plainToInstance(metadata.metatype, value);
+        return isMetaClass(metadata.metatype)
+            ? DataMapper.create(metadata.metatype, value)
+            : plainToInstance(metadata.metatype, value);
     }
 }
