bd8e75ed0911fdfc24fd7cd27e64dba68ec7f4f6
krydenk93 <krydenk@gmail.com>
2022-04-08 12:34:51 +0700

    1.3.25 Обновлено TimeField - добавлена проверка на nullable, сообщение об ошибке валидации сделано человекочитаемым

--- src/infrastructure/decorators/fields/TimeField.ts
+++ src/infrastructure/decorators/fields/TimeField.ts
@@ -1,21 +1,26 @@
 import {applyDecorators} from '@nestjs/common';
 import {Column} from 'typeorm';
-import {IsMilitaryTime} from 'class-validator';
+import {IsMilitaryTime, ValidateIf} from 'class-validator';
 import {BaseField, IBaseFieldOptions} from './BaseField';
 
 export function TimeField(options: IBaseFieldOptions = {}) {
     return applyDecorators(
-        BaseField(options, {
-            decoratorName: 'TimeField',
-            appType: 'time',
-            jsType: 'string',
-        }),
-        Column({
-            type: 'varchar',
-            length: 5,
-            default: options.defaultValue,
-            nullable: options.nullable,
-        }),
-        IsMilitaryTime(),
+        ...[
+            BaseField(options, {
+                decoratorName: 'TimeField',
+                appType: 'time',
+                jsType: 'string',
+            }),
+            Column({
+                type: 'varchar',
+                length: 5,
+                default: options.defaultValue,
+                nullable: options.nullable,
+            }),
+            options?.nullable && ValidateIf((object, value) => value !== null),
+            IsMilitaryTime({
+                message: 'Время необходимо ввести в формате часы:минуты, например 07:32'
+            }),
+        ].filter(Boolean)
     );
 }
