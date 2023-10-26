8a11b3b781a5e7ac4ee75ac736a90d2d43fbc053
Vladislav Koktashev <vladislav54415@gmail.com>
2023-10-26 16:47:01 +0300

    v1.7.30 remove JSONBField validation

--- src/infrastructure/decorators/fields/JSONBField.ts
+++ src/infrastructure/decorators/fields/JSONBField.ts
@@ -1,11 +1,9 @@
 import {applyDecorators} from '@nestjs/common';
 import {Column} from '@steroidsjs/typeorm';
-import {IsJSON, IsOptional} from 'class-validator';
+import {IsOptional} from 'class-validator';
 import {BaseField, IBaseFieldOptions} from './BaseField';
 
-export interface IJSONBFieldOptions extends IBaseFieldOptions {
-    isJSONConstraintMessage?: string,
-}
+export interface IJSONBFieldOptions extends IBaseFieldOptions {}
 
 export function JSONBField(options: IJSONBFieldOptions = {}) {
     return applyDecorators(...[
@@ -20,10 +18,6 @@ export function JSONBField(options: IJSONBFieldOptions = {}) {
             default: options.defaultValue,
             nullable: options.nullable,
             array: options.isArray,
-        }),
-        IsJSON({
-            each: options.isArray,
-            message: options.isJSONConstraintMessage || 'Должен быть JSON',
         }),
         !options.required && IsOptional(),
     ].filter(Boolean));
