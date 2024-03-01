adc846234d254bdb9abb93d510bedf81f9351d14
Vladislav Koktashev <vladislav54415@gmail.com>
2024-03-01 15:48:02 +0300

    1.7.36 swagger RelationField array case fix

--- src/infrastructure/decorators/fields/BaseField.ts
+++ src/infrastructure/decorators/fields/BaseField.ts
@@ -196,7 +196,7 @@ export function BaseField(options: IBaseFieldOptions = null, internalOptions: II
                 description: options.label || undefined,
                 example: options.example || undefined,
                 required: options.nullable === false,
-                isArray: options.isArray,
+                isArray: options.isArray || internalOptions.isArray,
             }),
             options.transform && Transform(options.transform),
             options.required && IsNotEmpty({
