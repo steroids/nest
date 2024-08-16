cfca627f26048025db3d9b05a3d998b454df6cb9
Vladislav Koktashev <vladislav54415@gmail.com>
2024-08-16 09:01:09 +0300

    Validation refactoring (#15)
    * Validation refactoring
    
    * Validation refactoring fix
    
    * Validation refactoring fix 2

--- src/infrastructure/filters/ValidationExceptionFilter.ts
+++ src/infrastructure/filters/ValidationExceptionFilter.ts
@@ -1,39 +1,19 @@
 import {ExceptionFilter, Catch, ArgumentsHost, HttpStatus} from '@nestjs/common';
 import { Response } from 'express';
-import {ValidationError} from 'class-validator';
-import {ValidationException} from '../../usecases/exceptions/ValidationException';
+import {ValidationException} from '../../usecases/exceptions';
 
 @Catch(ValidationException)
 export class ValidationExceptionFilter implements ExceptionFilter {
-    parseErrors(errors) {
-        if (Array.isArray(errors) && errors[0] instanceof ValidationError) {
-            return errors.reduce(
-                (result: any, item) => {
-                    if (item.constraints) {
-                        result[item.property] = [].concat(Object.values(item.constraints));
-                    }
-                    if (item.children?.length > 0) {
-                        result[item.property] = this.parseErrors(item.children);
-                    }
-                    return result;
-                },
-                {},
-            );
-        }
-        return errors;
-    }
-
     catch(exception: ValidationException, host: ArgumentsHost) {
         const ctx = host.switchToHttp();
         const response = ctx.getResponse<Response>();
-        const errors = this.parseErrors(exception.errors);
 
         response
             // TODO#Warning - ставим код 200, чтобы форма на фронте принимала массив ошибок
             .status(HttpStatus.OK)
             .json({
                 statusCode: HttpStatus.BAD_REQUEST,
-                errors,
+                errors: exception.errors,
             });
     }
 }
---
+++ src/usecases/interfaces/IErrorsCompositeObject.ts
@@ -0,0 +1,3 @@
+export interface IErrorsCompositeObject {
+    [propertyName: string]: string | string[] | IErrorsCompositeObject,
+}
