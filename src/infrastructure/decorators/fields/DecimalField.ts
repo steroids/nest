78bb2ee9d17294ae5f98639e31673d5a74148d68
Иванов Сергей <r1al1t@mail.ru>
2024-10-21 22:52:30 +0700

    add min max decorators decimal field

--- src/infrastructure/decorators/fields/DecimalField.ts
+++ src/infrastructure/decorators/fields/DecimalField.ts
@@ -1,12 +1,60 @@
 import {applyDecorators} from '@nestjs/common';
 import {Column} from '@steroidsjs/typeorm';
-import {IsDecimal, ValidateIf} from 'class-validator';
+import {buildMessage, IsDecimal, ValidateBy, ValidateIf, ValidationOptions} from 'class-validator';
 import {BaseField, IBaseFieldOptions} from './BaseField';
 
 export interface IDecimalFieldOptions extends IBaseFieldOptions {
     precision?: number,
     scale?: number,
     isDecimalConstraintMessage?: string,
+    minDecimalConstraintMessage?: string,
+    maxDecimalConstraintMessage?: string,
+}
+
+export const MIN_STRING_AS_NUMBER = 'minStringAsNumber';
+
+export const MAX_STRING_AS_NUMBER = 'maxStringAsNumber';
+
+function stringMin(num: unknown, min: number): boolean {
+    return typeof num === 'string' && typeof min === 'number' && Number(num) >= min;
+}
+
+function StringMin(minValue: number, validationOptions?: ValidationOptions): PropertyDecorator {
+    return ValidateBy(
+        {
+            name: MIN_STRING_AS_NUMBER,
+            constraints: [minValue],
+            validator: {
+                  validate: (value, args): boolean => stringMin(value, args?.constraints[0]),
+                  defaultMessage: buildMessage(
+                      eachPrefix => eachPrefix + '$property must not be less than $constraint1',
+                        validationOptions
+                  ),
+            },
+        },
+        validationOptions
+    );
+}
+
+function stringMax(num: unknown, max: number): boolean {
+    return typeof num === 'string' && typeof max === 'number' && Number(num) <= max;
+}
+
+function StringMax(maxValue: number, validationOptions?: ValidationOptions): PropertyDecorator {
+    return ValidateBy(
+        {
+            name: MAX_STRING_AS_NUMBER,
+            constraints: [maxValue],
+            validator: {
+                validate: (value, args): boolean => stringMax(value, args?.constraints[0]),
+                defaultMessage: buildMessage(
+                    eachPrefix => eachPrefix + '$property must not be greater than $constraint1',
+                    validationOptions
+                ),
+            },
+        },
+        validationOptions
+    );
 }
 
 export function DecimalField(options: IDecimalFieldOptions = {}) {
@@ -28,6 +76,14 @@ export function DecimalField(options: IDecimalFieldOptions = {}) {
                 decimal_digits: String(options.scale || 2),
             },{
                 message: options.isDecimalConstraintMessage || 'Должно быть числом',
+            }),
+            typeof options.min === 'number' && StringMin(options.min, {
+                each: options.isArray,
+                message: `Должно быть не меньше ${options.min}` || options.minDecimalConstraintMessage,
+            }),
+            typeof options.max === 'number' && StringMax(options.max, {
+                each: options.isArray,
+                message: `Должно быть не больше ${options.max}` || options.maxDecimalConstraintMessage,
             }),
         ].filter(Boolean)
     );
