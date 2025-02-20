e21040562efcd7cf95077e5a8efc0bcf207ed5cb
Danil <danil_ustinov@mail.ru>
2025-02-20 11:40:08 +0700

    fix: removed irrelevant comment

--- src/infrastructure/decorators/typeorm/TypeOrmTableFromModel.ts
+++ src/infrastructure/decorators/typeorm/TypeOrmTableFromModel.ts
@@ -16,9 +16,6 @@ function TypeOrmTableFromModelInternal(ModelClass: any) {
         setTableFromModel(ModelClass, target);
 
         getMetaFields(ModelClass).forEach(field => {
-            // считаем что поле модели полем таблицы, если для него задан декоратор,
-            // так как все steroids-поля задают декораторы
-            // @todo нужно определять является ли поле колонкой в бд более семантически логичным способом
             const options: IAllFieldOptions = getFieldOptions(ModelClass, field);
             const decoratorName = getFieldDecoratorName(ModelClass, field) as DecoratorFieldName;
             const isTableColumn = !!decoratorName;
