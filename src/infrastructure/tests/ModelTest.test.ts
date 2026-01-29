e2b39f0e24aaa8e20c2768c570e143ddbf972dc6
syomx <drophere.box@gmail.com>
2026-01-29 18:00:41 +0700

    fix test and run after create pr

--- src/infrastructure/tests/ModelTest.test.ts
+++ src/infrastructure/tests/ModelTest.test.ts
@@ -4,31 +4,30 @@ import {UserService} from './app/services/UserService';
 import {DataMapper} from '../../usecases/helpers/DataMapper';
 import {UserModel} from './app/models/UserModel';
 import {FileService} from './app/services/FileService';
-import {FileModel} from './app/models/FileModel';
 import SearchQuery from '../../usecases/base/SearchQuery';
-import {UserInfoModel} from './app/models/UserInfoModel';
 import {ImageService} from './app/services/ImageService';
-import {ImageModel} from './app/models/ImageModel';
 import {ArticleService} from './app/services/ArticleService';
-import {ArticleModel} from './app/models/ArticleModel';
-import {TagModel} from './app/models/TagModel';
 import {ArticleSchema} from './app/schemas/ArticleSchema';
+import {ImageSaveDto} from './app/dtos/ImageSaveDto';
+import {FileSaveDto} from './app/dtos/FileSaveDto';
+import {UserSaveDto} from './app/dtos/UserSaveDto';
+import {UserInfoSaveDto} from './app/dtos/UserInfoSaveDto';
+import {ArticleSaveDto} from './app/dtos/ArticleSaveDto';
+import {TagSaveDto} from './app/dtos/TagSaveDto';
 
-const createPhoto = (app) => {
-    return app.get(FileService).create(DataMapper.create<FileModel>(FileModel, {
-        name: 'file ' + Date.now(),
-        images: [
-            {
-                size: 'thumbnail',
-                url: 'https://test.com/image-thumbnail.png',
-            },
-            {
-                size: 'full',
-                url: 'https://test.com/image-full.png',
-            },
-        ],
-    }));
-}
+const createPhoto = (app) => app.get(FileService).create(DataMapper.create<FileSaveDto>(FileSaveDto, {
+    name: 'file ' + Date.now(),
+    images: [
+        {
+            size: 'thumbnail',
+            url: 'https://test.com/image-thumbnail.png',
+        },
+        {
+            size: 'full',
+            url: 'https://test.com/image-full.png',
+        },
+    ],
+}));
 
 describe('ModelTest', () => {
     let app;
@@ -42,15 +41,15 @@ describe('ModelTest', () => {
      */
     it('OneToMany ids', async () => {
         // Create file and image models with OneToMany::ids relations
-        const additionalImage1 = await app.get(ImageService).create(DataMapper.create<ImageModel>(ImageModel, {
+        const additionalImage1 = await app.get(ImageService).create(DataMapper.create<ImageSaveDto>(ImageSaveDto, {
             size: 'thumbnail',
             url: 'https://test.com/image-thumbnail.png',
         }));
         expect(additionalImage1?.id).toBeGreaterThan(0);
 
-        let additionalPhoto = await app.get(FileService).create(DataMapper.create<FileModel>(FileModel, {
+        let additionalPhoto = await app.get(FileService).create(DataMapper.create<FileSaveDto>(FileSaveDto, {
             name: 'file ' + Date.now(),
-            imagesIds: [additionalImage1.id]
+            imagesIds: [additionalImage1.id],
         }));
         expect(additionalPhoto?.id).toBeGreaterThan(0);
         expect(additionalPhoto?.imagesIds?.length).toEqual(1);
@@ -60,7 +59,7 @@ describe('ModelTest', () => {
             .findOne(
                 new SearchQuery()
                     .with(['imagesIds'])
-                    .where({id: additionalPhoto.id})
+                    .where({id: additionalPhoto.id}),
             );
 
         expect(additionalPhoto?.id).toBeGreaterThan(0);
@@ -74,21 +73,21 @@ describe('ModelTest', () => {
     it('ManyToMany data', async () => {
         const passportScan = await createPhoto(app);
 
-        let user: UserModel = await app.get(UserService).create(DataMapper.create<UserModel>(UserModel, {
+        const user: UserModel = await app.get(UserService).create(DataMapper.create<UserSaveDto>(UserSaveDto, {
             name: 'Test user ' + Date.now(),
-            info: DataMapper.create<UserInfoModel>(UserInfoModel, {
+            info: DataMapper.create<UserInfoSaveDto>(UserInfoSaveDto, {
                 passport: '0409 123456',
                 passportScanId: passportScan.id,
             }),
         }));
 
         // Create file and image models with OneToMany::ids relations
-        let article = await app.get(ArticleService).create(DataMapper.create<ArticleModel>(ArticleModel, {
+        let article = await app.get(ArticleService).create(DataMapper.create<ArticleSaveDto>(ArticleSaveDto, {
             title: 'How to load relations in entities',
             creatorUserId: user.id,
             tags: [
-                DataMapper.create<TagModel>(TagModel, {title: 'Tag 1'}),
-                DataMapper.create<TagModel>(TagModel, {title: 'Tag 2'}),
+                DataMapper.create<TagSaveDto>(TagSaveDto, {title: 'Tag 1'}),
+                DataMapper.create<TagSaveDto>(TagSaveDto, {title: 'Tag 2'}),
             ],
         }));
         expect(article?.id).toBeGreaterThan(0);
@@ -102,7 +101,6 @@ describe('ModelTest', () => {
         expect(article?.title).toEqual('HOW TO LOAD RELATIONS IN ENTITIES');
         expect(article?.shortTitle).toEqual('How t');
     });
-
 
     /**
      * Want test relations saves:
@@ -128,14 +126,15 @@ describe('ModelTest', () => {
         }
 
         // Create user: test ManyToOne::ids, ManyToMany::ids, OneToOne::data
-        let user: UserModel = await app.get(UserService).create(DataMapper.create<UserModel>(UserModel, {
+        let user: UserModel = await app.get(UserService).create(DataMapper.create<UserSaveDto>(UserSaveDto, {
             name: 'Test user ' + Date.now(),
-            info: DataMapper.create<UserInfoModel>(UserInfoModel, {
+            info: DataMapper.create<UserInfoSaveDto>(UserInfoSaveDto, {
                 passport: '0409 123456',
             }),
             mainPhotoId: mainPhoto.id,
             galleryPhotosIds: photos.map(photo => photo.id),
         }));
+
         expect(user?.id).toBeGreaterThan(0);
         expect(user?.mainPhotoId).toEqual(mainPhoto.id);
         expect(user?.galleryPhotosIds.length).toEqual(photos.length);
@@ -146,7 +145,7 @@ describe('ModelTest', () => {
             .findOne(
                 new SearchQuery()
                     .with(['mainPhotoId', 'galleryPhotosIds', 'info'])
-                    .where({id: user.id})
+                    .where({id: user.id}),
             );
         expect(user?.id).toBeGreaterThan(0);
         expect(user?.mainPhotoId).toEqual(mainPhoto.id);
