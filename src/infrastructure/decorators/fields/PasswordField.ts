15ac7de1016dd2cf99c6d8a5dfd75ae7ed703b76
Danil <danil_ustinov@mail.ru>
2026-02-12 12:20:23 +0700

    feat: use IsStrongPassword decorator in PasswordField

--- src/infrastructure/decorators/fields/PasswordField.ts
+++ src/infrastructure/decorators/fields/PasswordField.ts
@@ -1,16 +1,43 @@
 import {applyDecorators} from '@nestjs/common';
+import {IsStrongPassword} from 'class-validator';
 import {BaseField, IBaseFieldOptions} from './BaseField';
 
-export function PasswordField(options: IBaseFieldOptions = {}) {
-    if (!options.label) {
-        options.label = 'Пароль';
-    }
+export interface IPasswordFieldOptions extends IBaseFieldOptions {
+    minLength?: number,
+    minLowercase?: number,
+    minUppercase?: number,
+    minNumbers?: number,
+    minSymbols?: number,
+    isStrongPasswordConstraintMessage?: string,
+}
+
+export function PasswordField(options: IPasswordFieldOptions = {}) {
+    const finalOptions: IPasswordFieldOptions = {
+        label: 'Пароль',
+        minLength: 8,
+        minLowercase: 1,
+        minUppercase: 1,
+        minNumbers: 1,
+        minSymbols: 0,
+        isStrongPasswordConstraintMessage: 'Ненадёжный пароль',
+        ...options,
+    };
 
     return applyDecorators(
-        BaseField(options, {
+        BaseField(finalOptions, {
             decoratorName: 'PasswordField',
             appType: 'password',
             jsType: 'string',
+        }),
+        IsStrongPassword({
+            minLength: finalOptions.minLength,
+            minLowercase: finalOptions.minLowercase,
+            minUppercase: finalOptions.minUppercase,
+            minNumbers: finalOptions.minNumbers,
+            minSymbols: finalOptions.minSymbols,
+        }, {
+            each: finalOptions.isArray,
+            message: finalOptions.isStrongPasswordConstraintMessage,
         }),
     );
 }
