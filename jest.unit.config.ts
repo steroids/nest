8e3317c31ae18552c1be9bea10241a34330d9777
wereez <denis03122002@mail.ru>
2026-05-08 09:45:17 +0700

    #236 add postinstall script to clear Jest cache

--- jest.unit.config.ts
+++ jest.unit.config.ts
@@ -3,7 +3,6 @@ import type {Config} from '@jest/types';
 const config: Config.InitialOptions = {
     preset: 'ts-jest',
     roots: ['<rootDir>/src'],
-    cache: false,
     testPathIgnorePatterns: ['<rootDir>/src/infrastructure/tests'],
 };
 export default config;
