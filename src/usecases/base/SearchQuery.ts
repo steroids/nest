94f466487981e0a5edd6617e3371d8aa7e47f55a
Sergey Ivanov <rialit1993@gmail.com>
2026-08-14 21:15:02 +0700

    fix: remove double quotes from SearchQuery.orderBy (#95)

--- src/usecases/base/SearchQuery.ts
+++ src/usecases/base/SearchQuery.ts
@@ -295,16 +295,16 @@ export default class SearchQuery<TModel> {
 
         if (!pathToField.length) {
             if (field.split('_')[0] === this._alias) {
-                return wrapInDoubleQuotes(field);
+                return field;
             }
-            return `${wrapInDoubleQuotes(this._alias)}.${wrapInDoubleQuotes(field)}`;
+            return `${this._alias}.${field}`;
         }
 
         if (pathToField.length === 1 && pathToField[0].split('_')[0] === this._alias) {
-            return `${wrapInDoubleQuotes(pathToField[0])}.${wrapInDoubleQuotes(field)}`;
+            return `${pathToField[0]}.${field}`;
         }
 
-        return `${wrapInDoubleQuotes(this.getRelationAlias(pathToField.join('.')))}.${wrapInDoubleQuotes(field)}`;
+        return `${this.getRelationAlias(pathToField.join('.'))}.${field}`;
     }
 
     private resolveOrderByFieldPaths(
